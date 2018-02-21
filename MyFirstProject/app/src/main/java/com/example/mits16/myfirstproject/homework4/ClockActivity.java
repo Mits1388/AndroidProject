1bec2ec3de48d84eb9982193e0dce394d6e91720
Mits1388 <mits1388@gmail.com>
2018-02-21 21:55:18 +0300

    homework

--- MyFirstProject/app/src/main/java/com/example/mits16/myfirstproject/homework4/ClockActivity.java
+++ MyFirstProject/app/src/main/java/com/example/mits16/myfirstproject/homework4/ClockActivity.java
@@ -1,12 +1,11 @@
 package com.example.mits16.myfirstproject.homework4;
 
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-import com.example.mits16.myfirstproject.R;
 
-import java.util.Date;
 
 /**
  * Created by mizz8 on 18.02.2018.
@@ -14,14 +13,11 @@ import java.util.Date;
 
 public class ClockActivity extends AppCompatActivity {
 
-    private Date date;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         CustomView customView = new CustomView(this);
-        date = new Date();
         setContentView(customView);
-
     }
 }
